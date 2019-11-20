8ffeee25f6ce0c3859c1fbd6f8d4c52b36ab5e9f
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-20 21:46:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,6 +11,7 @@ class CashRegister
      def apply_discount
          discount = @total * (@discount / 100.0)
          @total-=discount
+         puts "After the discount, the total comes to $800."
      end
 end
 
