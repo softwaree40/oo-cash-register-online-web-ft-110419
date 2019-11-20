6998f3eb301940ba97cd1d6e57a699a54264d54a
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-20 22:45:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
      end
      def add_item(title,price,quantity = 1)
         @total+=price * quantity
-        
+        binding.pry
      end
      def apply_discount
          discount = @total * (@discount.to_f / 100)
@@ -23,6 +23,6 @@ class CashRegister
         
         
     end
-    binding.pry
+   # binding.pry
 end
 
